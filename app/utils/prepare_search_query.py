51649bc8b74e414961e6e4bfd699bab0d3cf5bbf
John <rezamosavian1014@gmail.com>
2023-11-09 15:46:33 +0330

    feat : add option for searching list on custome field

--- app/utils/prepare_search_query.py
+++ app/utils/prepare_search_query.py
@@ -4,7 +4,7 @@ from fastapi import Request
 def prepare_query_params_for_custome_field(request: Request):
     _static_query_filter_parameters = ("name", "status", "skip", "limit")
     return {
-        key: value
+        key: value if "," not in value else value.split(",")
         for key, value in dict(request.query_params).items()
         if key not in _static_query_filter_parameters
     }
